84df0281156331deebaddacc6a4edba80940049e
lion253 <144469478+lion253@users.noreply.github.com>
2024-01-28 21:40:10 +0400

    Add files via upload

---
+++ Module 7/main.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+from module import get_class
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def yes(ctx):
+    if ctx.message.attachments:
+        for i in ctx.message.attachments:
+            file_name = i.filename
+            file_url = i.url
+            await i.save(f"./{i.filename}")
+            await ctx.send (get_class(model_path='keras_model.h5', labels_path="labels.txt", image_path=f"./{i.filename}"))
+    else:
+        while True:
+            await ctx.send("@everyone Вы забили загрузить картинку ._.")
+
+bot.run("MTE1NDgwMDM4NTQ3NTc1NjEzNA.G7kgS1.PrQNFeivQD_2hysVS-q-3-1K3dteeObosaIvx8")
